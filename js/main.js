2429ad8d8e09e26dac6099979a32475ca2df66dd
aleksandrgolovichov <aleksandrgolovicev@gmail.com>
2025-06-15 19:51:19 +0300

    40%

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ const nav = document.getElementById('navMenu');
 const overlay = document.getElementById('menuOverlay');
 
 burger.addEventListener('click', () => {
-  nav.classList.add('active');
-  overlay.classList.add('active');
+  nav.classList.toggle('active');
+  overlay.classList.toggle('active');
 });
 
 overlay.addEventListener('click', () => {
   nav.classList.remove('active');
   overlay.classList.remove('active');
 });
+
